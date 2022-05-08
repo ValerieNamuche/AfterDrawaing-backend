b97b994515b42afbf7cfc0049586e0a48e90389b
judai <ernesvz@hotmail.com>
2022-05-07 22:12:52 -0500

    adding some configuration in security

--- backend-api/src/main/java/com/afterdrawing/backendapi/configuration/SecurityConfiguration.java
+++ backend-api/src/main/java/com/afterdrawing/backendapi/configuration/SecurityConfiguration.java
@@ -19,8 +19,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import   org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-
+import java.util.Arrays;
 
 
 @Configuration
@@ -42,6 +45,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .cors()
+                .and()
                 .csrf()
                 .disable()
                 .authorizeRequests()
@@ -58,7 +63,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
 
-
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     
 
 
@@ -79,8 +94,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     // provider for password encoder and user details service
-    
-
 
 
     @Bean
